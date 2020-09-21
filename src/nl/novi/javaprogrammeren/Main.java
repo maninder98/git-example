169bab2981c0dc70929ce220ae92de21a57fe2da
singm02 <msbenipal70@gmail.com>
2020-09-21 10:13:52 +0200

    commit2

--- src/nl/novi/javaprogrammeren/Main.java
+++ src/nl/novi/javaprogrammeren/Main.java
@@ -3,6 +3,6 @@ package nl.novi.javaprogrammeren;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Worldddd!");
     }
 }
